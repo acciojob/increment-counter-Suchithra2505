68892f2089fab795e3b867f982c90a272eb16305
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-07 17:30:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let p = document.getElementById('counter');
+let butt = document.getElementById('incrementBtn');
+
+let count = 0;
+
+butt.addEventListener('click', increse)
+function increse(){
+    alert (count);
+    count++;
+    p.innerHTML = count;
+}
